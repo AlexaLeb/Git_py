ba9351640a8c847841ee375f07baa142857e0eee
AlexaLeb <118658187+AlexaLeb@users.noreply.github.com>
2023-02-19 21:50:57 +0300

    1

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Color:
 
 
 def file_reader(file_path) -> list:
-    '''
+    """
     Функция принимает в качестве параметра путь к файлу с вопросами. Важно, чтобы вопросы в файле были оформлены
     определенным образом. На выходе функция выдает список словарей, каждый словарь имеет ключи:
     - номер вопроса
@@ -28,7 +28,7 @@ def file_reader(file_path) -> list:
     В конце функция перемешивает вопросы, чтобы в будущем те шли  у людей в рандомном порядке
     :param file_path:
     :returnsw as:
-    '''
+    """
     file = open(file_path, 'r',)
     dictionaries = []
     text = []
@@ -51,12 +51,30 @@ def file_reader(file_path) -> list:
 
 def questioner(dictionaries):
     player_score = 0
-    player_name = input(Color.BOLD +'Добро пожаловать в нашу викторину по Python! Пожалуйства, введите ваше имя: ' + Color.END)
+    overall = {}
+    print(Color.BLUE + Color.BOLD + 'Добро пожаловать в нашу викторину по Python! Хочешь принять учаситие и опробовать свои силы?')
+    while True:
+        welcome = input(Color.BLUE + Color.BOLD + 'Напиши "да" или "нет" ' + Color.END)
+        if welcome.lower() == 'да':
+            break
+        else:
+            print(Color.BLUE + Color.BOLD + r.choice(['Может все-таки поучавствуешь?', 'Мы же старались(((', 'Подумай еще', 'Приходи, когда передумаешь' + Color.END]))
+    player_name = input(Color.BOLD + 'Добро пожаловать в нашу викторину по Python! Пожалуйства, введите ваше имя: ' + Color.END)
+    overall['name'] = player_name
+    counter = 0
     for question in dictionaries:
-        player_score += asker(question)
-
-
-    return f'Викторина окончена', player_score, player_name
+        ask = asker(question)
+        player_score += ask[0]
+        answer = ask[1]
+        counter += 1
+        dict_with_aswr = {
+            'question': question['variants'],
+            'answer of player': answer,
+            'right answer': question['answer']
+        }
+        overall[f'question number {counter}'] = dict_with_aswr
+    overall['overall score'] = player_score
+    return f'Викторина окончена', overall
 
 def asker(question):
     score = int(question['score'])
@@ -66,10 +84,10 @@ def asker(question):
         answer = input(Color.DARKCYAN + 'Ваш ответ: ' + Color.END)
         if set(answer) == set(question['answer']):
             print(Color.GREEN + 'Поздравляю, вы абсолютно правы!' + Color.END)
-            return score
+            return score, answer
         elif tries == 1:
             print(Color.RED + Color.BOLD + f'К сожалению, вы так и не дали правильного ответа.\nВаши попытки кончились.' + Color.END)
-            return 0
+            return 0, answer
         else:
             tries -= 1
             print(Color.YELLOW + 'Вы неправы' + Color.RED + '!!!' + Color.END, f'\nПопробуйте еще раз')
